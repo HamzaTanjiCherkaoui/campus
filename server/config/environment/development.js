e98f41f1279b93515ac0597a96901138490b6143
hamza tanji cherkaoui <hoxa1235@gmail.com>
2015-06-28 23:41:17 +0100

    generating angular fullstack boilerplate

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/campus-dev'
+  },
+
+  seedDB: true
+};

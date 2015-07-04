0d72dd36eecf397ca4bd2823ad221fac2cc4646f
hamza tanji cherkaoui <hoxa1235@gmail.com>
2015-07-04 12:46:14 +0100

    deleting the cross site request forgery segment

--- server/config/express.js
+++ server/config/express.js
@@ -29,17 +29,7 @@ module.exports = function(app) {
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
-  app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-    
-    if ('OPTIONS' === req.method){
-        return res.send(200);
-    }
-    next();
-  });
+  
   
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));

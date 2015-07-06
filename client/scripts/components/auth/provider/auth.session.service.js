dca1509667765218b32d29b2232d93bfa23b660f
ben med <benaich.med@gmail.com>
2015-07-06 16:17:31 +0000

    commit for no reason

--- client/scripts/components/auth/provider/auth.session.service.js
+++ client/scripts/components/auth/provider/auth.session.service.js
@@ -1,32 +1,23 @@
 'use strict';
 
 angular.module('membershipApp')
-    .factory('AuthServerProvider', function loginService($http, localStorageService, $window) {
+    .factory('AuthServerProvider', function loginService($http, $cookieStore) {
         return {
             login: function(credentials) {
-                var data = 'j_username=' + encodeURIComponent(credentials.username) +
-                    '&j_password=' + encodeURIComponent(credentials.password) +
-                    '&_spring_security_remember_me=' + credentials.rememberMe + '&submit=Login';
-                return $http.post('api/authentication', data, {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
-                }).success(function (response) {
-                    localStorageService.set('token', $window.btoa(credentials.username + ':' + credentials.password));
-                    return response;
+                return $http.post('/auth/local', {
+                  email: credentials.email,
+                  password: credentials.password
+                }).
+                success(function(data) {
+                  $cookieStore.put('token', data.token);
+                  return data;
                 });
             },
             logout: function() {
-                // logout from the server
-                $http.post('api/logout').success(function (response) {
-                    localStorageService.clearAll();
-                    // to get a new csrf token call the api
-                    $http.get('api/account');
-                    return response;
-                });
+                $cookieStore.remove('token');
             },
             getToken: function () {
-                var token = localStorageService.get('token');
+                var token = $cookieStore.get('token');
                 return token;
             },
             hasValidToken: function () {
